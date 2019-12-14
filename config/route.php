55220ff204e375abcaa0695f219690d976ca9c8a
Sara Iacobelli <sara88.iacobelli@gmail.com>
2019-12-14 17:33:52 +0100

    aggiunta Home alla route

--- config/route.php
+++ config/route.php
@@ -3,6 +3,7 @@ use SimpleMVC\Controller;
 
 return [
     'GET /' => Controller\Home::class, // "PHPFinalProject\Controller\Home",
+	'GET /Home' => Controller\Home::class, // "PHPFinalProject\Controller\Home",
     'GET /Article' => Controller\Article::class, // "PHPFinalProject\Controller\Article\{id}",
 	'GET /Login' => Controller\Login::class, // "PHPFinalProject\Controller\Login",
     'GET /Register' => Controller\Register::class, // "PHPFinalProject\Controller\Register",
