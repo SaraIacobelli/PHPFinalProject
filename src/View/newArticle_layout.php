c26621e62066b2e3e1ebc675c8e7885c68831dd4
Lorenzo <lorenzo.bert99@gmail.com>
2019-12-17 11:04:17 +0100

    Aggiunta pagina NewArticle

---
+++ src/View/newArticle_layout.php
@@ -0,0 +1,20 @@
+<?php $this->layout('layout', ['title' => 'newArticle']) ?>
+<div style="width: 50%; margin: auto;">
+	<h1 style="text-align: center">Registrati</h1>
+	<div  style="display: flex; justify-content: space-between;">
+    <div class="container">
+    <h1>Nuovo articolo</h1>
+    
+    <form name='titolo' action='dati' method='POST'>
+    <label for="titolo"><b>Titolo</b></label>
+    <input type="text" placeholder="Inserisci un titolo" name="titolo" required>
+
+    <label for="data"><b>Data</b></label>
+    <input type="text" placeholder="Inserisci la data" name="data" required>
+
+    <label for="testo"><b>Testo articolo</b></label>
+    <input type="text" placeholder="Inserisci il testo dell'articolo" name="testo" required>
+
+    <button type="submit" class="newbtn">Crea</button>
+  </div>
+</form>
